5ecd0d3463f5ce1a758ddbdf14ee203a1b881ad4
minseong <decidone@gmail.com>
2019-12-16 04:50:13 +0900

    플레이어 낙하, 낙하 후 이동까지 추가, 이동할때 위치가 어긋나는 문제 수정

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Vector3 offset;
     public GameObject player;
     public GameObject center;
     public GameObject up;
@@ -13,39 +12,35 @@ public class Movement : MonoBehaviour
     public GameObject right;
     public GameObject rightUp;
     public int step = 9;
-    private Vector3 lastPos;
-
-    // 움직이고 난 후 아래 블록이 있는지 검사
-    public bool isFloating;
+    public Vector3 gravity;
+    
     public float fallingSpeed = -0.1f;
-
     public float speed = (float)0.01;
-    bool input;
+
+    bool input;         // 플레이어의 이동모션이 끝났는지 체크
+    bool ismoving;      // 큐브가 움직이고 있는지 체크
+    bool isFloating;    // 움직이고 난 후 아래 블록이 있는지 검사
+    float timer;        // ismoving 체크에 딜레이를 걸어줌
+    Vector3 lastPos;
+
     // Start is called before the first frame update
     void Start()
     {
         isFloating = false;
         input = true;
+        ismoving = false;
     }
 
     void FixedUpdate()
     {
-        //Vector3 r = transform.TransformDirection(Vector3.right);
+        CheckChangePosition();
+        CheckUnderBlock(center.transform.position + new Vector3(0, -1.2f, 0), 0.1f);
 
-        //if (Physics.Raycast(center.transform.position + new Vector3(0.5f, 0, 0), r, 1))
-        //    print("There is something in front of the object!");
-
-        //if (Physics.CheckSphere(center.transform.position + new Vector3(0, -1.1f, 0), 0.1f))
-        //{
-        //    Debug.Log("뭔가 있음");
-        //}
         if (input == true)
         {
             center.transform.position = player.transform.position;
-            //Debug.Log("떨어져요");
-            //StartCoroutine("fall");
         }
-        CheckUnderBlock(center.transform.position + new Vector3(0, -1.2f, 0), 0.1f);
+        
     }
 
     void CheckUnderBlock(Vector3 centerPoint, float radius)
@@ -58,7 +53,7 @@ public class Movement : MonoBehaviour
         }
         while (i < Colliders.Length)
         {
-            Debug.Log(Colliders[i].tag);
+            // Debug.Log(Colliders[i].tag);
             if(Colliders[i].tag == "Wall")
             {
                 isFloating = false;
@@ -66,14 +61,7 @@ public class Movement : MonoBehaviour
             else
             {
                 isFloating = true;
-                //if(input == true)
-                //{
-                //    center.transform.position = player.transform.position;
-                //    Debug.Log("떨어져요");
-                //    //StartCoroutine("fall");
-                //}
             }
-            //hitColliders[i].SendMessage("AddDamage");
             i++;
         }
     }
@@ -81,7 +69,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (input == true && isFloating == false)
+        if (input == true && isFloating == false && ismoving == false)
         {
             if (Input.GetKey(KeyCode.UpArrow)){
                 StartCoroutine("moveUp");
@@ -110,21 +98,6 @@ public class Movement : MonoBehaviour
         }
     }
 
-    IEnumerator fall()
-    {
-        for (int i = 0; i < (10); i++)
-        {
-            player.transform.TransformPoint(new Vector3(0, -0.1f, 0));
-            //Physics.gravity = new Vector3(0, 0, 0);
-            //player.transform.(up.transform.position, Vector3.right, step);
-            yield return new WaitForSeconds(speed);
-        }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
-        center.transform.position = player.transform.position;
-        input = true;
-        //Debug.Log("up");
-    }
-
     IEnumerator moveUp()
     {
         for(int i=0; i<(90/step); i++)
@@ -133,7 +106,7 @@ public class Movement : MonoBehaviour
             player.transform.RotateAround(up.transform.position, Vector3.right, step);
             yield return new WaitForSeconds(speed);
         }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = gravity;
         center.transform.position = player.transform.position;
         input = true;
         //Debug.Log("up");
@@ -147,7 +120,7 @@ public class Movement : MonoBehaviour
             player.transform.RotateAround(down.transform.position, Vector3.left, step);
             yield return new WaitForSeconds(speed);
         }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = gravity;
         center.transform.position = player.transform.position;
         input = true;
     }
@@ -160,7 +133,7 @@ public class Movement : MonoBehaviour
             player.transform.RotateAround(left.transform.position, Vector3.forward, step);
             yield return new WaitForSeconds(speed);
         }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = gravity;
         center.transform.position = player.transform.position;
         input = true;
     }
@@ -173,7 +146,7 @@ public class Movement : MonoBehaviour
             player.transform.RotateAround(right.transform.position, Vector3.back, step);
             yield return new WaitForSeconds(speed);
         }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = gravity;
         center.transform.position = player.transform.position;
         input = true;
     }
@@ -186,8 +159,27 @@ public class Movement : MonoBehaviour
             player.transform.RotateAround(rightUp.transform.position, Vector3.back, step);
             yield return new WaitForSeconds(speed * 2f);
         }
-        Physics.gravity = new Vector3(0, -9.8f, 0);
+        Physics.gravity = gravity;
         center.transform.position = player.transform.position;
         input = true;
+    }
+
+    public void CheckChangePosition()
+    {
+        timer += Time.deltaTime;
+        // 0.05초 전과 현재 위치를 비고
+        if (timer >= 0.05f)
+        {
+            if (lastPos != player.transform.localPosition)
+            {
+                ismoving = true;
+                lastPos = player.transform.localPosition;
+            }
+            else
+            {
+                ismoving = false;
+            }
+            timer = 0f;
+        }
     }
 }
