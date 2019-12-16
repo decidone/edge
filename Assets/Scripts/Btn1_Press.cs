c40fec5d493e03f1ebbfa48e225f6ea1529ed8e2
minseong <decidone@gmail.com>
2019-12-17 06:11:10 +0900

    블록 이동 애니메이션 추가

--- Assets/Scripts/Btn1_Press.cs
+++ Assets/Scripts/Btn1_Press.cs
@@ -6,6 +6,7 @@ public class Btn1_Press : MonoBehaviour
 {
     bool ispressed;
     public GameObject blocks;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +26,27 @@ public class Btn1_Press : MonoBehaviour
             if (ispressed == false)
             {
                 transform.position = new Vector3(transform.position.x, 0.26f, transform.position.z);
-                blocks.transform.position
-                    = new Vector3(blocks.transform.position.x + 2, blocks.transform.position.y, blocks.transform.position.z);
+
+                StartCoroutine(MovingBlocks(blocks.transform.position, new Vector3(blocks.transform.position.x + 2,
+                    blocks.transform.position.y, blocks.transform.position.z), 30));
+
                 ispressed = true;
             }
         }
+    }
+
+    IEnumerator MovingBlocks(Vector3 before, Vector3 after, int time)
+    {
+        Vector3 diff;
+        diff = after - before;
+        for (int i = 0; i < time; i++)
+        {
+            blocks.transform.position
+                = new Vector3(blocks.transform.position.x + (diff.x / time), blocks.transform.position.y + (diff.y / time), blocks.transform.position.z + (diff.z / time));
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+        //blocks.transform.position = after;
+        
     }
 }
