c29877545523a0fca8809e440c49227d04dc6043
minseong <decidone@gmail.com>
2019-12-17 05:52:41 +0900

    블럭 이동처리

---
+++ Assets/Scripts/Btn1_Press.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btn1_Press : MonoBehaviour
+{
+    bool ispressed;
+    public GameObject blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ispressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (ispressed == false)
+            {
+                transform.position = new Vector3(transform.position.x, 0.26f, transform.position.z);
+                blocks.transform.position
+                    = new Vector3(blocks.transform.position.x + 2, blocks.transform.position.y, blocks.transform.position.z);
+                ispressed = true;
+            }
+        }
+    }
+}
