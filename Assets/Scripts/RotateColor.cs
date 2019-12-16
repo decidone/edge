b7c3ac49f25d1fa91b159d22560b7525d782dc97
minseong <decidone@gmail.com>
2019-12-16 11:14:58 +0900

    savePoint 색상 추가

---
+++ Assets/Scripts/RotateColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateColor : MonoBehaviour
+{
+    public float time;
+    float colorstep;
+    Color[] colors = new Color[4];
+    int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors[0] = Color.red;
+        colors[1] = Color.yellow;
+        colors[2] = Color.cyan;
+        colors[3] = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colorstep < time)
+        {
+            GetComponent<Renderer>().material.color = Color.Lerp(colors[i], colors[i + 1], colorstep);
+            colorstep += 0.02f;
+        }
+        else
+        {
+            colorstep = 0;
+            if (i < (colors.Length - 2))
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+    }
+}
