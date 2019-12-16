6701d4ed8c3d693c443e0d0739aece6090677bd7
minseong <decidone@gmail.com>
2019-12-17 05:29:44 +0900

    button prefab 추가

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    bool ispressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ispressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(ispressed == false)
+            {
+                transform.position = new Vector3(transform.position.x, 0.26f, transform.position.z);
+            }
+        }
+    }
+}
